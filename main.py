bfba92159b61aa09e1876b0c788661dd35f28362
hamegg123-spec <ham_egg123@yahoo.co.jp>
2026-05-16 11:54:06 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 import threading
 import discord
 from discord.ext import commands
+import os
 
-bot = commands.Bot(command_prefix="!")
+intents = discord.Intents.default()
+intents.message_content = True  # メッセージ内容を読む場合は必須
+
+bot = commands.Bot(command_prefix="!", intents=intents)
 
 @bot.event
 async def on_ready():
     print("Bot is ready")
 
 def run_bot():
-    bot.run("DISCORD_TOKEN")
+    bot.run(os.getenv("DISCORD_TOKEN"))
 
-# Bot を別スレッドで起動
 threading.Thread(target=run_bot).start()
